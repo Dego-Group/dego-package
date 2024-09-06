278794c43905bfd61eba9445dc98a38747c8c4ec
Hexy32 <sunshine.goody8762@gmail.com>
2024-09-06 16:07:26 -0400

    Made spacing even

--- default-project/src/pages/about.ts
+++ default-project/src/pages/about.ts
@@ -16,25 +16,19 @@ export default function About() {
       "React itself doesn't natively support multiple pages. You'll need additional libraries or frameworks like Next.js to achieve multi-page functionality. React's reliance on runtime UI generation can negatively impact SEO.",
     ],
 
-    // Dego vs. React Comparison - Feature 3: Build Dependencies
-    [
-      'Dego boasts zero build dependencies. The Dego team maintains complete control over the code shipped to users, resulting in smaller bundle sizes and potentially faster hydration times.',
-      "React utilizes third-party dependencies. Frameworks like Next.js used for matching Dego's functionality introduce additional dependencies, increasing bundle size.",
-    ],
-
-    // Dego vs. React Comparison - Feature 4: Load Times
+    // Dego vs. React Comparison - Feature 3: Load Times
     [
       'Dego excels in load times, achieving speeds like 48ms for initial content rendering. This is due to pre-generating HTML at build time and using hydration for interactivity. Dego employs various optimization techniques for consistent performance across devices and network conditions.',
       "React lacks a comparable feature. It relies on the user's device for rapid loading because JS on the client generates all HTML. React also sends substantial JavaScript on initial load, which Dego avoids.",
     ],
 
-    // Dego vs. React Comparison - Feature 5: Developer Experience (Boilerplate Code)
+    // Dego vs. React Comparison - Feature 4: Developer Experience (Boilerplate Code)
     [
       'Dego minimizes boilerplate code to streamline developer workflow. It offers clean, concise syntax for common tasks.',
       'Many React features have specific usage requirements, potentially leading to excessive boilerplate code.',
     ],
 
-    // Dego vs. React Comparison - Feature 6: State Management
+    // Dego vs. React Comparison - Feature 5: State Management
     [
       'Dego incorporates built-in, user-friendly mechanisms for global state management and other core functionalities, eliminating the need for external libraries and simplifying project setup.',
       'React requires additional, often complex third-party libraries to handle global state management and other essential features, adding to the learning curve.',
